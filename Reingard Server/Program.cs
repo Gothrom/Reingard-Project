366f794625f43dc7d96538c5a22fea5aa9c7c917
Gothrom <rr_gothrom@redrodgers.com>
2020-10-09 21:06:27 +0300

    Add Reingard Server project

---
+++ Reingard Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Reingard_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Reingard World!");
+            Console.WriteLine("I am Reingard Server, at Your Grace.");
+            Console.ReadKey();
+        }
+    }
+}
