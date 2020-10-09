f9f7fa3388c0071eeb48d1b8c272e090ad387253
Gothrom <rr_gothrom@redrodgers.com>
2020-10-09 21:12:05 +0300

    Add Reingard Client

---
+++ Reingard Client/Reingard Client.cpp
@@ -0,0 +1,14 @@
+// Reingard Client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello from Reingard World!\n";
+    std::cout << "I'm Reingard Client, my Master\n";
+    std::cin.get();
+
+    return 0;
+}
+
